3c68e274f4b57213830fc356990fafa46acea716
Pedro David López Correa <pedro_correa23@hotmail.com>
2023-01-31 19:56:24 -0600

    MARKII

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { GifsService } from '../services/gifs.service';
+
+
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styleUrls: ['./busqueda.component.css']
 })
 export class BusquedaComponent {
 
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  constructor( private gifsService: GifsService ) {} 
+
+  Buscar() {
+
+    const valor = this.txtBuscar.nativeElement.value;
+      
+      this.gifsService.BuscarGifs ( valor ); 
+
+      this.txtBuscar.nativeElement.value = ''; 
+  }
 }
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,24 @@
+import { ReturnStatement } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+    private _historial: string [] = [];
+
+    get historial () {
+      return [...this._historial];
+    }
+
+    BuscarGifs( query: string ) {
+
+      this._historial.unshift ( query );
+
+      console.log(this._historial);
+      
+       
+    }
+
+}
